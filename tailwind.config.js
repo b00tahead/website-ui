4677a7240fc907089459dc36c8eeddd9f5dcfdca
Scott May <smay44@gmail.com>
2021-03-23 21:04:55 -0400

    Customize tailwind theme and add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: {
+        DEFAULT: "#16161A",
+      },
+      white: {
+        DEFAULT: "#FFFFFE",
+      },
+      green: {
+        DEFAULT: "#2CB67D",
+      },
+    },
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      display: ["Poppins"],
+    },
   },
   variants: {
     extend: {},
