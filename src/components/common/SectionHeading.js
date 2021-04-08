0b0269062e9552f8509c21243a86ed772b2ae970
Scott May <smay44@gmail.com>
2021-04-07 21:14:15 -0400

    Create a common components folder

---
+++ src/components/common/SectionHeading.js
@@ -0,0 +1,9 @@
+function SectionHeading(props) {
+  return (
+    <h2 className="font-display font-bold text-green uppercase text-sm mb-3">
+      {props.heading}
+    </h2>
+  );
+}
+
+export default SectionHeading;
