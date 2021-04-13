3d8f303240b6a9f75bcbbe5504951237287b9463
Scott May <smay44@gmail.com>
2021-04-13 18:38:30 -0400

    Add new header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Data from "./data";
-// import Section from "./components/Section.js";
+
+import Header from "./components/Header";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-  const sectionSpacing = "py-24";
+  const sectionSpacing = "py-20";
 
   return (
     <div>
+      <Header />
       <section className={sectionSpacing}>
         <h1 className="font-display font-black text-2xl text-center">
           Hello, I'm <span className="text-green">Scott</span>.
